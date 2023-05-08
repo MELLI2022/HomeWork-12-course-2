4e6de9c6ea4993573193277e7801cdd1ec9f2132
MELLI2022 <denisenkoswetlana109@gmail.com>
2023-05-08 14:01:47 +0300

    Мой первый коммит

---
+++ src/main/java/com/example/homework12course2/HomeWork12Course2Application.java
@@ -0,0 +1,13 @@
+package com.example.homework12course2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork12Course2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWork12Course2Application.class, args);
+    }
+
+}
